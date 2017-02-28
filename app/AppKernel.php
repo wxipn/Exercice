eceabf545e9566ec4dc6debb724773a8331dd69a
wxipn <wxipn@free.fr>
2017-02-28 11:28:41 +0000

    Modification

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-            new SEB\ParserBundle\SEBParserBundle(),
+            new SEB\ParserBundle\SEBParserBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -24,7 +23,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-			//$bundles[] = new Symfony\Component\DomCrawler\Crawler();
         }
 
         return $bundles;
